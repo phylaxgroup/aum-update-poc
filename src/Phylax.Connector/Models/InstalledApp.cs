3410fb72d98b6843cf5dabf41946211034d548e7
phylaxgroup <chris.judkins@phylaxgroup.com>
2026-08-05 20:27:00 -0600

    Update InstalledApp.cs

---
+++ src/Phylax.Connector/Models/InstalledApp.cs
@@ -0,0 +1,9 @@
+namespace Phylax.Connector.Models;
+
+public record InstalledApp(
+    string DisplayName,
+    string DisplayVersion,
+    string Publisher,
+    string? ProductCode,
+    string? WingetId
+);
