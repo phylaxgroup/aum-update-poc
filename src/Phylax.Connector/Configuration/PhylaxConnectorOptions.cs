3bd8d4718439a7410d7ab8ebaaf3c976eef1f366
phylaxgroup <chris.judkins@phylaxgroup.com>
2026-08-05 19:56:43 -0600

    Update PhylaxConnectorOptions.cs

---
+++ src/Phylax.Connector/Configuration/PhylaxConnectorOptions.cs
@@ -0,0 +1,8 @@
+namespace Phylax.Connector.Configuration;
+
+public class PhylaxConnectorOptions
+{
+    public string TenantId { get; set; } = string.Empty;
+    public string ApiBaseUrl { get; set; } = string.Empty;
+    public string ApiKey { get; set; } = string.Empty;
+}
