b356acc8e64fad99aad12c31401aba3cdaaba2a7
phylaxgroup <chris.judkins@phylaxgroup.com>
2026-08-09 11:03:41 -0600

    Update CatalogUpdate.cs

---
+++ src/Phylax.Connector/Models/CatalogUpdate.cs
@@ -0,0 +1,8 @@
+namespace Phylax.Connector.Models;
+
+public class CatalogUpdate
+{
+    public string AppName { get; set; } = string.Empty;
+    public string TargetVersion { get; set; } = string.Empty;
+    public string KbArticle { get; set; } = string.Empty;
+}
