1f7c963104b5ad1a09c2ae0ba614dde56b0b7b7b
phylaxgroup <chris.judkins@phylaxgroup.com>
2026-08-12 19:26:25 -0600

    Refactor Program.cs to use HostBuilder

--- src/Phylax.FunctionApp/Program.cs
+++ src/Phylax.FunctionApp/Program.cs
@@ -1,29 +1,19 @@
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Phylax.FunctionApp.Services;
 
-var builder = FunctionsApplication.CreateBuilder(args);
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+        
+        services.AddSingleton<WingetManifestService>();
+        services.AddSingleton<VersionComparisonService>();
+        services.AddSingleton<LogAnalyticsIngestionService>();
+    })
+    .Build();
 
-// Satisfies the ASP.NET Core integration requirements for extension packages
-builder.ConfigureFunctionsWebApplication();
-
-// Default HttpClient
-builder.Services.AddHttpClient();
-
-// Named client for winget GitHub API calls
-builder.Services.AddHttpClient("winget", client =>
-{
-    client.DefaultRequestHeaders.UserAgent.ParseAdd("PhylaxFunctionApp/1.0");
-    client.DefaultRequestHeaders.Accept.ParseAdd(
-        "application/vnd.github.v3+json");
-    client.Timeout = TimeSpan.FromSeconds(30);
-});
-
-// Core backend services
-builder.Services.AddSingleton<WingetManifestService>();
-builder.Services.AddSingleton<VersionComparisonService>();
-builder.Services.AddSingleton<LogAnalyticsIngestionService>(); // Replaced InventoryStorageService
-
-builder.Build().Run();
+host.Run();
