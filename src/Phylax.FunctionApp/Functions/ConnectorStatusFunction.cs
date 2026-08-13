2cb327e4ab919bbfca840505af6e2369300b3447
phylaxgroup <chris.judkins@phylaxgroup.com>
2026-08-12 21:14:31 -0600

    Update ConnectorStatusFunction.cs

--- src/Phylax.FunctionApp/Functions/ConnectorStatusFunction.cs
+++ src/Phylax.FunctionApp/Functions/ConnectorStatusFunction.cs
@@ -1,35 +1,14 @@
-using System.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
-using Microsoft.Extensions.Logging;
-using Phylax.FunctionApp.Models;
 
 namespace Phylax.FunctionApp.Functions;
 
 public class ConnectorStatusFunction
 {
-    private readonly ILogger<ConnectorStatusFunction> _logger;
-
-    public ConnectorStatusFunction(ILogger<ConnectorStatusFunction> logger)
-    {
-        _logger = logger;
-    }
-
     [Function("ConnectorStatus")]
-    public async Task<HttpResponseData> Run(
-        [HttpTrigger(AuthorizationLevel.Function, "post", Route = "connector/status")] HttpRequestData req)
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "connector/status")] HttpRequest req)
     {
-        _logger.LogInformation("Receiving connector telemetry status update.");
-
-        var status = await req.ReadFromJsonAsync<ConnectorStatusRequest>();
-        if (status is null)
-        {
-            return req.CreateResponse(HttpStatusCode.BadRequest);
-        }
-
-        _logger.LogInformation("Connector Execution Summary -> Machine: {Machine}, App: {App}, Success: {Status}", 
-            status.MachineName, status.ApplicationName, status.Success);
-
-        return req.CreateResponse(HttpStatusCode.OK);
+        return new OkResult();
     }
 }
